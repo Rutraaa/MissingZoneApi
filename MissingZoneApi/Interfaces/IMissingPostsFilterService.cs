8c4a91fdc375d5a2d6379758eb6fc3667354b306
Hryhoriy K. <101716956+geloft@users.noreply.github.com>
2024-05-12 01:16:19 +0300

    Add MissingPosts Filter Service

---
+++ MissingZoneApi/Interfaces/IMissingPostsFilterService.cs
@@ -0,0 +1,10 @@
+﻿using MissingZoneApi.Contracts.MissingPost;
+using MissingZoneApi.Entities;
+
+namespace MissingZoneApi.Interfaces
+{
+    public interface IMissingPostsFilterService
+    {
+        IEnumerable<MissingPost> FilterMissingPosts(IEnumerable<MissingPost> posts, GetAllMissingPostsRequest request);
+    }
+}
