6d0a1b309bd41d2e5bc7d80e4b211db72e89c001
Hryhoriy K. <101716956+geloft@users.noreply.github.com>
2024-05-12 03:23:50 +0300

    Add validation for LoginRequest

--- MissingZoneApi/Contracts/AuthReg/LoginRequest.cs
+++ MissingZoneApi/Contracts/AuthReg/LoginRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace MissingZoneApi.Contracts.AuthReg;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MissingZoneApi.Contracts.AuthReg;
 
 public class LoginRequest
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    [Required]
     public string Password { get; set; }
 }
